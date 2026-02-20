bb705baf1a36fa1d83eb10c280515e5c324e9431
Jair Meza <jair3202@gmail.com>
2026-02-20 18:19:47 -0500

    export CreateFile and remove SavePID

--- internal/files.go
+++ internal/files.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strconv"
 )
 
 type FileManager struct {
@@ -29,7 +28,7 @@ func (fm *FileManager) InitDir() error {
 	return nil
 }
 
-func (fm *FileManager) createFile(fileName string) (io.WriteCloser, error) {
+func (fm *FileManager) CreateFile(fileName string) (io.WriteCloser, error) {
 	file, err := os.Create(filepath.Join(fm.basePath, fileName))
 	if err != nil {
 		return nil, fmt.Errorf("couldn't create %s: %w", fileName, err)
@@ -38,29 +37,14 @@ func (fm *FileManager) createFile(fileName string) (io.WriteCloser, error) {
 }
 
 func (fm *FileManager) CreateLogFiles() (stdOut, stdErr io.Writer, err error) {
-	outFile, err := fm.createFile("out.log")
+	outFile, err := fm.CreateFile("out.log")
 	if err != nil {
 		return nil, nil, err
 	}
 
-	errFile, err := fm.createFile("err.log")
+	errFile, err := fm.CreateFile("err.log")
 	if err != nil {
 		return nil, nil, err
 	}
 	return outFile, errFile, nil
-}
-
-func (fm *FileManager) SavePID(pid int) error {
-	file, err := fm.createFile("pid.txt")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	pidStr := strconv.Itoa(pid)
-	_, err = file.Write([]byte(pidStr))
-	if err != nil {
-		return err
-	}
-	return nil
 }
