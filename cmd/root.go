e32a58658a621fa89ebe166003fc0eb7384fafbd
Jair Meza <jair3202@gmail.com>
2026-02-20 00:45:16 -0500

    initial scaffolding

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "devserve",
+	Short: "Serve JavaScript projects with Tailscale",
+	Long:  `Serve your local dev servers across your Tailscale network with devserve.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
