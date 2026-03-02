99ed3041d5d15adee856fd120d9c3279b1fdb3d6
Jair Meza <jair3202@gmail.com>
2026-03-02 18:55:38 -0500

    added restart command

---
+++ cmd/restart.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"devserve/cli"
+	"devserve/daemon"
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var restartCmd = &cobra.Command{
+	Use:   "restart [name]",
+	Args:  cobra.ExactArgs(1),
+	Short: "Restart a running process",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+
+		// Stop the process
+		req := &daemon.Request{
+			Action: "stop",
+			Args: map[string]any{
+				"name": name,
+			},
+		}
+		var (
+			resp *daemon.Response
+			err  error
+		)
+		cli.Spin("Stopping process...", func() {
+			resp, err = daemon.Send(req)
+		})
+		if err != nil {
+			return fmt.Errorf("failed to send stop request: %w", err)
+		}
+		if !resp.OK {
+			return errors.New(resp.Error)
+		}
+
+		// Start it again from saved config
+		return runStart(name)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(restartCmd)
+}
